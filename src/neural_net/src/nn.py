6e080bd51d175b8365eb0ce2d4b2b852873b01a5
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2020-10-08 16:46:17 -0500

    updated nn

--- src/neural_net/src/nn.py
+++ src/neural_net/src/nn.py
@@ -2,25 +2,39 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
+import rosbag
+import pandas as pd
 
-model = Sequential()
-model.add(Dense(units=100, activation='relu'))
-model.add(Dense(units=10))
+bag = rosbag.Bag('./') #bag file
+topic = #topic
+column_names = 
+df = pd.DataFrame(columns=column_names)
+for topic, msg, t in bag.read_messages(topics=topic):
+	x = msg.x
+	y = msg.y
+	df = df.append(
+	)
 
-train_X = [[1] * 100] * 100
-test_X = [[1] * 100] * 100
+df.to_csv('out.csv')
 
-train_Y = [[1] * 10] * 100
-test_Y = [[1] * 10] * 100
+# model = Sequential()
+# model.add(Dense(units=100, activation='relu'))
+# model.add(Dense(units=10))
 
-# print(test_Y)
+# train_X = [[1] * 100] * 100
+# test_X = [[1] * 100] * 100
 
-opt = SGD(lr = .1, momentum = 0.9)
+# train_Y = [[1] * 10] * 100
+# test_Y = [[1] * 10] * 100
 
-model.compile(loss='mean_squared_error',
-    optimizer=opt,
-    metrics=['mean_squared_error'])
+# # print(test_Y)
 
-model.fit(train_X, train_Y, epochs=100, batch_size=32)
+# opt = SGD(lr = .1, momentum = 0.9)
 
-loss_and_metrics = model.evaluate(test_X, test_Y, batch_size=32)
+# model.compile(loss='mean_squared_error',
+#     optimizer=opt,
+#     metrics=['mean_squared_error'])
+
+# model.fit(train_X, train_Y, epochs=100, batch_size=32)
+
+# loss_and_metrics = model.evaluate(test_X, test_Y, batch_size=32)
