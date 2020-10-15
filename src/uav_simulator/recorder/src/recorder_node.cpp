f8f3a907b9fde790faccdcd3fc390420f84908c5
Pranav Rayudu <tarakapranav@gmail.com>
2020-10-15 15:23:50 -0500

    first opencv build

---
+++ src/uav_simulator/recorder/src/recorder_node.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void depthmapCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "recorder");
+
+  ros::NodeHandle n;
+
+  cv::namedWindow("view");
+  cv::startWindowThread();
+  
+  image_transport::ImageTransport it(n);
+  
+  image_transport::Subscriber sub = it.subscribe("/pcl_render_node/depth", 1000, depthmapCallback);
+
+  ros::spin();
+
+  return 0;
+}
