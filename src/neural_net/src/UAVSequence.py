a77a2253069e0d11fcd95f444e83c417ae8958df
akashsingh1 <akashsinghr3@gmail.com>
2020-12-10 16:04:03 -0600

    Updated nn code, new transformations on both inputs and outputs, untested

--- src/neural_net/src/UAVSequence.py
+++ src/neural_net/src/UAVSequence.py
@@ -10,35 +10,27 @@ import math
 # complete this to fit into nn
 class ImageSequence(Sequence):
 
-    def __init__(self, x_set, batch_size, img_shape, hash_table, validation_split, is_validation):
+    def __init__(self, x_set, batch_size, img_shape, hash_table, offset=0):
         self.x = x_set
         self.batch_size = batch_size
         self.img_shape = img_shape
         print('batch is {}'.format(self.batch_size))
         self.hash_table = hash_table
-        self.validation_split = validation_split
-        self.is_validation = is_validation
+        self.offset = offset
         
     def __len__(self):
         num_batches = len(self.x) / self.batch_size
-        if self.is_validation:
-            num_batches *= self.validation_split
-        else:
-            num_batches *= 1 - self.validation_split
         
         return int(num_batches)
     
-    def __getitem__(self, idx):
-        
-        if self.is_validation:
-            idx += int(len(self.x) * (1 - self.validation_split) / self.batch_size)
-        
+    def __getitem__(self, idx): 
         batch_x = self.x[idx * self.batch_size: (idx + 1) * self.batch_size]
         img_x = np.zeros((self.batch_size, *self.img_shape, 1))
 
         for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
-            image = Image.open('../dataset/img_' + str(self.hash_table[i]) + '.jpg')
-            imagearr = np.asarray(image).reshape(*self.img_shape, 1) 
+#             image = Image.open('../dataset_transformed_12_7/img_' + str(self.hash_table[i] + self.offset) + '.jpg'
+#             imagearr = np.asarray(image).reshape(*self.img_shape, 1) 
+            imagearr = np.zeros((480, 640, 1))
             
             img_x[i % self.batch_size] = imagearr / 255
 
@@ -46,37 +38,31 @@ class ImageSequence(Sequence):
         
 class UAVSequence(Sequence):
 
-    def __init__(self, x_set, y_set, batch_size, img_shape, hash_table, validation_split, is_validation):
+    def __init__(self, x_set, y_set, batch_size, img_shape, hash_table, offset=0):
         self.x, self.y = x_set, y_set
+        
         self.batch_size = batch_size
         self.img_shape = img_shape
         print('batch is {}'.format(self.batch_size))
         self.hash_table = hash_table
-        self.validation_split = validation_split
-        self.is_validation = is_validation
+        self.offset = offset
         
     def __len__(self):
         num_batches = len(self.x) / self.batch_size
-        if self.is_validation:
-            num_batches *= self.validation_split
-        else:
-            num_batches *= 1 - self.validation_split
         
         return int(num_batches)
     
     def __getitem__(self, idx):
-        
-        if self.is_validation:
-            idx += int(len(self.x) * (1 - self.validation_split) / self.batch_size)
-        
         batch_x = self.x[idx * self.batch_size: (idx + 1) * self.batch_size]
         batch_y = self.y[idx * self.batch_size: (idx + 1) * self.batch_size]
+        
         img_x = np.zeros((self.batch_size, *self.img_shape, 1))
 
         for i in range(idx * self.batch_size, (idx + 1) * self.batch_size):
-            image = Image.open('../dataset/img_' + str(self.hash_table[i]) + '.jpg')
-            imagearr = np.asarray(image).reshape(*self.img_shape, 1) 
+#             image = Image.open('../dataset_transformed_12_7/img_' + str(self.hash_table[i] + self.offset) + '.jpg')
+#             imagearr = np.asarray(image).reshape(*self.img_shape, 1) 
             
+            imagearr = np.zeros((480, 640, 1))
             img_x[i % self.batch_size] = imagearr / 255
 
         return [img_x, np.array(batch_x)], np.array(batch_y)
