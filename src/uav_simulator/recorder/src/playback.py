6d293c5c72c1a7481af98444844cde310fdc05b5
Pranav Rayudu <tarakapranav@gmail.com>
2020-12-15 17:34:15 -0600

    added all recorder changes

--- src/uav_simulator/recorder/src/playback.py
+++ src/uav_simulator/recorder/src/playback.py
@@ -10,6 +10,8 @@ import pandas as pd
 import numpy as np
 import math
 from PIL import Image
+import transforms3d
+import os
 
 import rospy
 from std_msgs.msg import String
@@ -26,6 +28,32 @@ from cv_bridge import CvBridge, CvBridgeError
 import csv
 
 
+os.system("rosnode kill traj_server")
+# def model_loss(target, predicted):
+#     print(target.shape)
+#     print(predicted.shape)
+#     position_loss = tf.keras.losses.MSE(target[:,:3], predicted[:,:3])
+#     other_loss = tf.keras.losses.MSE(target[:,3:], predicted[:,3:])
+    
+#     total_loss = 10 * position_loss + other_loss
+#     return total_loss
+
+def pos_metric(y_true, y_pred):
+    squared_difference = tf.square(y_true - y_pred)
+    return tf.reduce_mean(squared_difference[:3], axis=-1)
+
+def vel_metric(y_true, y_pred):
+    squared_difference = tf.square(y_true - y_pred)
+    return tf.reduce_mean(squared_difference[3:6], axis=-1)
+
+def acc_metric(y_true, y_pred):
+    squared_difference = tf.square(y_true - y_pred)
+    return tf.reduce_mean(squared_difference[6:9], axis=-1)
+
+def yaw_metric(y_true, y_pred):
+    squared_difference = tf.square(y_true - y_pred)
+    return tf.reduce_mean(squared_difference[9:], axis=-1)
+	
 # NUM_INPUTS = 13
 # NUM_OUTPUTS = 9
 
@@ -49,8 +77,45 @@ import csv
 graph = tf.get_default_graph()
 session = tf.compat.v1.keras.backend.get_session()
 
-model_complete = tf.keras.models.load_model('/home/pranavr/ego-planner/src/uav_simulator/recorder/models/complete.tf')
 
+model_complete = tf.keras.models.load_model('/home/pranavr/ego-planner/src/uav_simulator/recorder/models/complete_empty_30_min_no_image_0_050.tf',
+											custom_objects={"pos_metric": pos_metric, "vel_metric": vel_metric, "acc_metric": acc_metric, "yaw_metric": yaw_metric})#, custom_objects={"model_loss": model_loss})
+
+# model_complete = tf.keras.models.load_model('/home/pranavr/ego-planner/src/uav_simulator/recorder/models/complete_empty_30_min_transformed_0_034.tf',
+# 											custom_objects={"pos_metric": pos_metric, "vel_metric": vel_metric, "acc_metric": acc_metric, "yaw_metric": yaw_metric})#, custom_objects={"model_loss": model_loss})
+
+
+# input_mn = [ 0.00071267,  0.00121005, -0.01057223, -0.03100285, -0.04670384, -0.00124892,
+#   0.00200639,  0.00305923, -0.01772598]
+# input_std = [0.05952703, 0.1066482,  0.47686612, 1.10708861, 1.24799304, 0.12653448,
+#  0.37974445, 0.37144707, 0.63460594]
+# output_mn = [ 0.00038266, -0.00076545,  0.00295317, -0.03242625, -0.04784536, -0.00096541,
+#   0.01030061,  0.00485227, -0.00516944, -0.00201966, -0.02447285]
+# output_std = [0.04649806, 0.03959377, 0.00973197, 1.11215682, 1.25204454, 0.12756201,
+#  0.62285087, 0.80529237, 0.20998748, 0.06998235, 0.92470723]
+
+input_mn = [ 0.0035498,  -0.02925752, -0.02631224, -0.02401414, -0.00523067, -0.00217564,
+ -0.0010349,   0.00572376, -0.03676773]
+input_std = [0.16396395, 0.18420277, 0.93733231, 1.20875131, 1.25781257, 0.24468761,
+ 0.86837934, 0.89024787, 1.27144478]
+
+output_mn = [ 2.65993210e-03,  1.52750180e-03,  1.39032816e-02, -2.26173063e-02,
+ -5.06012831e-03,  2.72522870e-05, -1.00951970e-02, -4.60337859e-03,
+ -1.37203478e-02, -3.80694860e-03, -5.90423143e-02]
+output_std = [0.08561747, 0.09262722, 0.02173819, 1.2201406,  1.27785797, 0.24312548,
+ 1.43014532, 1.55238503, 0.55457504, 0.14860682, 1.90875224]
+
+# dataset = "/home/pranavr/ego-planner/src/uav_simulator/recorder/dataset/"
+
+
+
+def clamp_yaw(diff):
+    if diff > math.pi:
+        diff -= 2 * math.pi
+    elif diff < -1 * math.pi:
+        diff += 2 * math.pi
+    
+    return diff
 
 # print(xy[1][1][0])
 # single_x_test = xy[1][0]
@@ -118,53 +183,64 @@ def callback(img, odom):
 	global goal_set
 	global current_goal
 
-	if not goal_set:
-		rospy.loginfo("[POS CMD: [%f, %f, %f]\n", 
-				odom.pose.pose.position.x,
-				odom.pose.pose.position.y,
-				odom.pose.pose.position.z,
-			)
+	# if not goal_set:
+	# 	rospy.loginfo("[POS CMD: [%f, %f, %f]\n", 
+	# 			odom.pose.pose.position.x,
+	# 			odom.pose.pose.position.y,
+	# 			odom.pose.pose.position.z,
+	# 		)
 		
 
 	if goal_set:
-		print(current_goal)
+		# print(current_goal)
 		orientation = [
 			odom.pose.pose.orientation.x,
 			odom.pose.pose.orientation.y,
 			odom.pose.pose.orientation.z,
 			odom.pose.pose.orientation.w
 		]
-		rospy.loginfo("[POS CMD: [%f, %f, %f], [%f, %f, %f, %f]\n", 
-				odom.pose.pose.position.x,
-				odom.pose.pose.position.y,
-				odom.pose.pose.position.z,
+		# rospy.loginfo("[POS CMD: [%f, %f, %f], [%f, %f, %f, %f]\n", 
+		# 		odom.pose.pose.position.x,
+		# 		odom.pose.pose.position.y,
+		# 		odom.pose.pose.position.z,
 				
-				odom.pose.pose.orientation.x,
-				odom.pose.pose.orientation.y,
-				odom.pose.pose.orientation.z,
-				odom.pose.pose.orientation.w
-			)
+		# 		odom.pose.pose.orientation.x,
+		# 		odom.pose.pose.orientation.y,
+		# 		odom.pose.pose.orientation.z,
+		# 		odom.pose.pose.orientation.w
+		# 	)
 		
 		roll, pitch, yaw = euler_from_quaternion(orientation)
 		rel_x, rel_y = current_goal.position.x - odom.pose.pose.position.x, \
-					current_goal.position.y - odom.pose.pose.position.y
-		angle = math.atan2(rel_y, rel_x) - yaw
+						current_goal.position.y - odom.pose.pose.position.y
+		angle = clamp_yaw(math.atan2(rel_y, rel_x) - yaw)
 		distance = math.sqrt(rel_y * rel_y + rel_x * rel_x)
-		distance = min(2, distance)
+		# distance = min(2, distance)
+		
+		# print(roll, pitch, yaw)
+		rot_matrix = np.asarray(transforms3d.euler.euler2mat(roll, pitch, yaw))
+		
+
+		# INPUTS
+		# linear velocities
+		rel_lin_vel = np.matmul(rot_matrix, [odom.twist.twist.linear.x, 
+											odom.twist.twist.linear.y,
+											odom.twist.twist.linear.z])
+		# angular velocities
+		rel_ang_vel = np.matmul(rot_matrix, [odom.twist.twist.angular.x, 
+											odom.twist.twist.angular.y,
+											odom.twist.twist.angular.z])
+
 
 		odom_input = np.asarray([
-			distance,
-			angle,
 			roll,
 			pitch,
-			yaw,
+			angle,
+			# distance,
+			# yaw,
 
-			odom.twist.twist.linear.x, 
-			odom.twist.twist.linear.y,
-			odom.twist.twist.linear.z,
-			odom.twist.twist.angular.x, 
-			odom.twist.twist.angular.y,
-			odom.twist.twist.angular.z  
+			*rel_lin_vel,
+			*rel_ang_vel 
 		])
 
 
@@ -173,26 +249,26 @@ def callback(img, odom):
 		cv_image = bridge.imgmsg_to_cv2(img, "32FC1")
 		cv_image = cv2.convertScaleAbs(cv_image, alpha=30)
 		# print(cv_image.size)
-		cv2.imshow("Image window", cv_image)
-		cv2.waitKey(3)
+		# cv2.imshow("Image window", cv_image)
+		# cv2.waitKey(3)
 
-		odom_input = np.asarray([
-			rel_x,
-			rel_y,
-			odom.pose.pose.position.z,
+		# odom_input = np.asarray([
+		# 	-rel_x,
+		# 	-rel_y,
+		# 	odom.pose.pose.position.z,
 			
-			odom.pose.pose.orientation.x,
-			odom.pose.pose.orientation.y,
-			odom.pose.pose.orientation.z,
-			odom.pose.pose.orientation.w,
+		# 	odom.pose.pose.orientation.x,
+		# 	odom.pose.pose.orientation.y,
+		# 	odom.pose.pose.orientation.z,
+		# 	odom.pose.pose.orientation.w,
 
-			odom.twist.twist.linear.x, 
-			odom.twist.twist.linear.y,
-			odom.twist.twist.linear.z,
-			odom.twist.twist.angular.x, 
-			odom.twist.twist.angular.y,
-			odom.twist.twist.angular.z  
-		])
+		# 	odom.twist.twist.linear.x, 
+		# 	odom.twist.twist.linear.y,
+		# 	odom.twist.twist.linear.z,
+		# 	odom.twist.twist.angular.x, 
+		# 	odom.twist.twist.angular.y,
+		# 	odom.twist.twist.angular.z  
+		# ])
 
 		# rospy.
 		# https://stackoverflow.com/questions/54652536/keras-tensorflow-backend-error-tensor-input-10-specified-in-either-feed-de
@@ -201,27 +277,88 @@ def callback(img, odom):
 		# odom_input = odom_input.reshape(1, len(odom_input))
 		cv_image = np.asarray(cv_image).reshape(480, 640, 1) / 255
 		# cv_image = np.expand_dims(cv_image, axis=0)
-		print(cv_image.shape)
-		print(odom_input.shape)
-		print()
+		# print(cv_image.shape)
+		# print(odom_input.shape)
+		# print()
 		cv_image = np.array([cv_image,])
 		odom_input = np.array([odom_input,])
-		print(cv_image.shape)
-		print(odom_input.shape)
+		# print(cv_image.shape)
+		# print(odom_input.shape)
 
 		# model_complete = tf.keras.models.load_model('/home/pranavr/ego-planner/src/uav_simulator/recorder/models/complete.tf')
 		# with graph.as_default():
-		with session.graph.as_default():
-			tf.compat.v1.keras.backend.set_session(session)
-			pos_cmd = model_complete.predict([cv_image, odom_input])
+	
+		if distance > 0.5:
+			with session.graph.as_default():
+				tf.compat.v1.keras.backend.set_session(session)
 
-			global id
-			print(pos_cmd.shape)
-			pos_cmd = pos_cmd[0]
-			pos_cmd = PositionCommand(None, Point(*pos_cmd[0:3]), Vector3(*pos_cmd[3:6]), Vector3(*pos_cmd[6:9]), pos_cmd[9], pos_cmd[10], [0, 0, 0], [0, 0, 0], id, PositionCommand.TRAJECTORY_STATUS_READY)
-			pos_cmd_publisher.publish(pos_cmd)
-			rospy.loginfo("[POS CMD: pos_cmd\ {} n".format(pos_cmd))
-		id += 1
+				# convert = (u - min) / (max - min)
+				# for i in range(len(odom_input)):
+				# 	odom_input[i] = (odom_input[i] - input_min[i]) / (input_max[i] - input_min[i])
+				for i in range(len(odom_input)):
+					odom_input[i] = (odom_input[i] - input_mn[i]) / input_std[i]
+				# pos_cmd = model_complete.predict([cv_image, odom_input])
+				pos_cmd = model_complete.predict(odom_input)
+
+				global id
+				# print(pos_cmd.shape)
+				rospy.loginfo("[POS CMD: pos_cmd\ {} n".format(pos_cmd))
+
+				# revert = u * (max - min) + min
+				pos_cmd = pos_cmd[0]
+				for i in range(len(pos_cmd)):
+					pos_cmd[i] = (pos_cmd[i] * output_std[i]) + output_mn[i]
+				
+				rev_rot_matrix = np.linalg.inv(rot_matrix)
+				rev_rot_matrix = transforms3d.euler.euler2mat(-roll, -pitch, -yaw)
+
+				print(transforms3d.euler.mat2euler(rot_matrix))
+				print(transforms3d.euler.mat2euler(rev_rot_matrix))
+				# print(np.around(rev_rot_matrix, 3))
+				# rev_rot_matrix = np.transpose(rot_matrix)
+				# print(np.around(rev_rot_matrix, 3))
+				# rev_rot_matrix = transforms3d.euler.euler2mat(-roll, -pitch, -yaw)
+				# print(np.around(rev_rot_matrix, 3))
+				# print(np.around(rot_matrix * rev_rot_matrix, 3))
+				# pos_cmd[0:3] = [0.1, 0, 0]
+				# pos = [0.1, 0, 0]
+				pos = np.dot(rev_rot_matrix, pos_cmd[0:3]) * 20
+				# pos *= -1
+				vel = np.dot(rev_rot_matrix, pos_cmd[3:6])
+				# vel *= -1
+				acc = np.dot(rev_rot_matrix, pos_cmd[6:9])
+				# print(pos.shape, vel.shape, acc.shape)
+				# acc[1] *= -1
+				# print(np.linalg.matrix_rank(rot_matrix))
+				# print(np.linalg.det(rot_matrix))
+				# print(transforms3d.euler.mat2euler(rev_rot_matrix))
+				# print(np.around(rot_matrix, 3))
+				# print(np.around(np.linalg.inv(rev_rot_matrix), 2))
+				# print(np.dot(rot_matrix, rev_rot_matrix))
+
+				con = math.pi / 10
+				yaw = clamp_yaw(yaw + min(max(-con, pos_cmd[9]), con))
+				# yaw = clamp_yaw(math.atan2(rel_y, rel_x))
+				pos_cmd = PositionCommand(
+					None, 
+					Point((pos[0] + odom.pose.pose.position.x),
+						  (pos[1] + odom.pose.pose.position.y),
+						  (pos[2] + odom.pose.pose.position.z)), 
+					# Point(0, 0, 0),
+					# Point(odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z), 
+					# Vector3(*vel / 5), 
+					Vector3(0, 0, 0),
+					# Vector3(*acc / 5),
+					Vector3(0, 0, 0),
+					yaw, 
+					# 0,
+					# pos_cmd[10] / 5,
+					0,
+					[0, 0, 0], [0, 0, 0], id, PositionCommand.TRAJECTORY_STATUS_READY)
+				
+				pos_cmd_publisher.publish(pos_cmd)
+				rospy.loginfo("[POS CMD: pos_cmd\ {} n".format(pos_cmd))
+			id += 1
 
 rospy.init_node('playback_node')
 
@@ -233,13 +370,6 @@ odom_sub = message_filters.Subscriber('/visual_slam/odom', Odometry)
 
 # rospy.Subscriber("/visual_slam/odom", Odometry, callback, buff_size=10)
 rospy.Subscriber("/move_base_simple/goal", PoseStamped, goalCallback, buff_size=10)
-
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd', PositionCommand, queue_size=10)
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd')
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd')
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd')
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd')
-# pos_cmd_publisher = rospy.Publisher('/planning/pos_cmd')
 
 # ts = message_filters.ApproximateTimeSynchronizer([image_sub, odom_sub], 5, 0.1, allow_headerless=True)
 ts = message_filters.ApproximateTimeSynchronizer([image_sub, odom_sub], 100, 1)
--- src/uav_simulator/recorder/src/recorder_node.cpp
+++ src/uav_simulator/recorder/src/recorder_node.cpp
@@ -20,7 +20,7 @@
 #include <string>
 
 int id = 0;
-std::string dataset_folder = "dataset";
+std::string dataset_folder = "dataset_empty_30_transformed_12_11";
 std::string input_data = dataset_folder + "/0_input.csv";
 std::string output_data = dataset_folder + "/0_output.csv";
 std::string output_imgs = dataset_folder + "/img_";
@@ -32,7 +32,11 @@ std::ofstream inputFile;
 std::ofstream outputFile;
 
 bool save = true;
-bool goal_set = false;
+bool img_save = false;
+bool auto_rec = true;
+
+ros::Publisher goal_cmd_pub;
+bool goal_set;
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
@@ -119,70 +123,123 @@ void goalCallback(const geometry_msgs::PoseStamped& goal) {
 }
 
 void callback(const sensor_msgs::ImageConstPtr& msg, const quadrotor_msgs::PositionCommandConstPtr& cmd, const nav_msgs::Odometry::ConstPtr& odom) {
-    // ROS_INFO("callback called\n");
+  // ROS_INFO("callback called, %f, %f\n", goal_set ? 1.0 : 0.0, odom == nullptr ? 0.0 : 1.0);
   // Write the contents of these two messages to a CSV file for training
   // Right now, just use every 100 pixels arbitrarily in the image, later on we need to run all of through some other sort of filter
   // Want to use two CSV files, one for input and one for output
   // std::ofstream imageFile;
   
-  ROS_INFO("[POS CMD: [%f, %f, %f], [%f, %f, %f, %f]\n", 
-    odom->pose.pose.position.x,
-    odom->pose.pose.position.y,
-    odom->pose.pose.position.z,
-    
-    odom->pose.pose.orientation.x,
-    odom->pose.pose.orientation.y,
-    odom->pose.pose.orientation.z,
-    odom->pose.pose.orientation.w);
+
 
   
   tf::Quaternion q(odom->pose.pose.orientation.x, odom->pose.pose.orientation.y, odom->pose.pose.orientation.z, odom->pose.pose.orientation.w);
-    tf::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-    std::cout << "Roll: " << roll << ", Pitch: " << pitch << ", Yaw: " << yaw << std::endl;
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+  // std::cout << "Roll: " << roll << ", Pitch: " << pitch << ", Yaw: " << yaw << std::endl;
 
 
-  if (save && goal_set && odom != nullptr) {
-    outputFile << 
-            std::to_string(cmd->position.x) + "," + std::to_string(cmd->position.y) + "," + std::to_string(cmd->position.z) + "," + 
+  if (goal_set && odom != nullptr) {
+
+    auto rel_y = (current_goal.position.y - odom->pose.pose.position.y);
+    auto rel_x = (current_goal.position.x - odom->pose.pose.position.x);
+    double angle = atan2(rel_y, rel_x) - yaw;
+    double distance = sqrt(rel_y * rel_y + rel_x * rel_x);
+    
+    // TODO:: consider imbalance of samples at exactly 2 and effect on training performance
+    // distance = distance > 2 ? 2 : distance; 
+    // ROS_INFO("dist is %f\n", distance);
+    if (distance > 0.5) {
+      if (save) {
+        // ROS_INFO("[POS CMD: [%f, %f, %f], [%f, %f, %f, %f]\n", 
+      //   odom->pose.pose.position.x,
+      //   odom->pose.pose.position.y,
+      //   odom->pose.pose.position.z, 
+        
+      //   odom->pose.pose.orientation.x,
+      //   odom->pose.pose.orientation.y,
+      //   odom->pose.pose.orientation.z,
+      //   odom->pose.pose.orientation.w);
+        outputFile << 
+            std::to_string(cmd->position.x) + "," + 
+            std::to_string(cmd->position.y) + "," + 
+            std::to_string(cmd->position.z) + "," + 
+            // std::to_string(cmd->position.x - odom->pose.pose.position.x) + "," + 
+            // std::to_string(cmd->position.y - odom->pose.pose.position.y) + "," + 
+            // std::to_string(cmd->position.z - odom->pose.pose.position.z) + "," + 
             std::to_string(cmd->velocity.x) + "," + std::to_string(cmd->velocity.y) + "," + std::to_string(cmd->velocity.z) + "," + 
             std::to_string(cmd->acceleration.x) + "," + std::to_string(cmd->acceleration.y) + "," + std::to_string(cmd->acceleration.z) + "," + 
             std::to_string(cmd->yaw) + "," + std::to_string(cmd->yaw_dot) + "\n";
-  
-    auto rel_y = (current_goal.position.y - odom->pose.pose.position.y);
-    auto rel_x =(current_goal.position.x - odom->pose.pose.position.x);
-    double angle = atan2(rel_y, rel_x) - yaw;
-    double distance = sqrt(rel_y * rel_y + rel_x * rel_x);
 
-    
-    
-    // TODO:: consider imbalance of samples at exactly 2 and effect on training performance
-    distance = distance > 2 ? 2 : distance; 
-
-    inputFile << 
+        inputFile << 
             // std::to_string(current_goal.position.x - odom->pose.pose.position.x) + "," + 
             // std::to_string(current_goal.position.y - odom->pose.pose.position.y) + "," +
-
-
-
-            // std::to_string(odom->pose.pose.position.z) + "," +
-            // std::to_string(odom->pose.pose.orientation.x) + "," + 
+            std::to_string(odom->pose.pose.position.x) + "," +
+            std::to_string(odom->pose.pose.position.y) + "," +
+            std::to_string(odom->pose.pose.position.z) + "," +
+            // std::to_string(odom->pose.pose.orientation.x) + "," +
             // std::to_string(odom->pose.pose.orientation.y) + "," +
             // std::to_string(odom->pose.pose.orientation.z) + "," +
             // std::to_string(odom->pose.pose.orientation.w) + "," +
-            std::to_string(distance) + "," + 
-            std::to_string(angle) + "," +
+          
             std::to_string(roll) + "," + 
-            std::to_string(pitch) + "," +
-            std::to_string(yaw) + "," +
+            std::to_string(pitch) + "," + 
+            std::to_string(yaw) + "," + 
+          
+            std::to_string(angle) + "," + 
+            std::to_string(distance) + "," + 
             // TODO:: consider the need to do frame transformations to the drone's coordinate frame with these
             std::to_string(odom->twist.twist.linear.x) + "," + 
-            std::to_string(odom->twist.twist.linear.y) + "," +
-            std::to_string(odom->twist.twist.linear.z) + "," +
+            std::to_string(odom->twist.twist.linear.y) + "," + 
+            std::to_string(odom->twist.twist.linear.z) + "," + 
             std::to_string(odom->twist.twist.angular.x) + "," + 
             std::to_string(odom->twist.twist.angular.y) + "," +
-            std::to_string(odom->twist.twist.angular.z) + "\n";    
+            std::to_string(odom->twist.twist.angular.z) + 
+            "\n";
+
+        if (img_save) {
+          cv_bridge::CvImageConstPtr cv_ptr;
+          try {
+            cv_ptr = cv_bridge::toCvShare(msg, "32FC1");
+            cv::Mat mono8_img = cv::Mat(cv_ptr->image.size(), CV_8UC1);
+            cv::convertScaleAbs(cv_ptr->image, mono8_img, 30, 0.0);
+            // cv::imshow("view", mono8_img);
+            // ROS_INFO("writing image\n");
+
+            // if (save && goal_set && odom != nullptr)
+            cv:imwrite(output_imgs + std::to_string(id) + output_ext, mono8_img);
+            cv::waitKey(20);
+          
+          } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("Could not convert from '%s' to '32FC1'.", msg->encoding.c_str());
+          }
+        }
+      }
+
+      id++;
+    } else if (auto_rec) {
+      ROS_INFO("Reached goal\n");
+
+      double candidate_x = odom->pose.pose.position.x, candidate_y = odom->pose.pose.position.y, dist = 0;
+      geometry_msgs::PoseStamped goal;
+
+      while (dist < 1 || dist > 5) {
+        
+        candidate_x = (rand() % 40) - 20;
+        candidate_y = (rand() % 40) - 20;
+        double off_x = candidate_x - odom->pose.pose.position.x, off_y = candidate_y - odom->pose.pose.position.y;
+        dist = sqrt(off_x * off_x + off_y * off_y);
+      }
+
+      goal.pose.position.x = candidate_x;
+      goal.pose.position.y = candidate_y;
+      goal.pose.orientation.w = 1;
+      // reset_goal = false;
+      goal_cmd_pub.publish(goal);
+      current_goal = goal.pose;
+          // ROS_INFO("callback called\n");
+      ROS_INFO("Goal set to [%f, %f]\n", candidate_x, candidate_y);
+    }
   }
 
   // ROS_INFO("[POS CMD %d: [%f, %f, %f], [%f, %f, %f], [%f, %f, %f], yaw: %f, %f\n", 
@@ -205,23 +262,7 @@ void callback(const sensor_msgs::ImageConstPtr& msg, const quadrotor_msgs::Posit
   //   cmd->yaw_dot
   // );
 
-  cv_bridge::CvImageConstPtr cv_ptr;
-  try
-  {
-    cv_ptr = cv_bridge::toCvShare(msg, "32FC1");
-    cv::Mat mono8_img = cv::Mat(cv_ptr->image.size(), CV_8UC1);
-    cv::convertScaleAbs(cv_ptr->image, mono8_img, 30, 0.0);
-    // cv::imshow("view", mono8_img);
-    // ROS_INFO("writing image\n");
 
-    if (save && goal_set && odom != nullptr)
-      cv:imwrite(output_imgs + std::to_string(id++) + output_ext, mono8_img);
-    cv::waitKey(20);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to '32FC1'.", msg->encoding.c_str());
-  }
 }
 
 int main(int argc, char **argv)
@@ -242,20 +283,29 @@ int main(int argc, char **argv)
   ROS_INFO("started node\n");
   // image_transport::Subscriber sub = it.subscribe("/pcl_render_node/depth", 1000, depthmapCallback);
 
-  ros::Subscriber inputSub = n.subscribe("/visual_slam/odom", 100, inputCallback);
+  // ros::Subscriber inputSub = n.subscribe("/visual_slam/odom", 100, inputCallback);
   // ros::Subscriber outputSub = n.subscribe("/planning/pos_cmd", 100, outputCallback);
+  // goal_set = auto_rec;
 
-  ros::Subscriber goal_cmd_sub = n.subscribe("/move_base_simple/goal", 10, goalCallback);
+  ros::Subscriber goal_cmd_sub;
+  if (auto_rec) {
+    goal_cmd_pub = n.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+    ROS_INFO("Automatic Goal Setting: true\n");
+  } else {
+      ROS_INFO("Automatic Goal Setting: false\n");
+  }
 
-  // message_filters::Subscriber<sensor_msgs::Image> image_sub(n, "/pcl_render_node/depth", 10);
-  // message_filters::Subscriber<quadrotor_msgs::PositionCommand> pos_cmd_sub(n, "/planning/pos_cmd", 10);
-  // message_filters::Subscriber<nav_msgs::Odometry> odom_sub(n, "/visual_slam/odom", 10);
+  goal_cmd_sub = n.subscribe("/move_base_simple/goal", 10, goalCallback);
+
+  message_filters::Subscriber<sensor_msgs::Image> image_sub(n, "/pcl_render_node/depth", 10);
+  message_filters::Subscriber<quadrotor_msgs::PositionCommand> pos_cmd_sub(n, "/planning/pos_cmd", 10);
+  message_filters::Subscriber<nav_msgs::Odometry> odom_sub(n, "/visual_slam/odom", 10);
   
-  // typedef sync_policies::ApproximateTime<sensor_msgs::Image, quadrotor_msgs::PositionCommand,nav_msgs::Odometry> Policy;
-  // message_filters::Synchronizer<Policy> sync(Policy(100), image_sub, pos_cmd_sub, odom_sub);
+  typedef sync_policies::ApproximateTime<sensor_msgs::Image, quadrotor_msgs::PositionCommand,nav_msgs::Odometry> Policy;
+  message_filters::Synchronizer<Policy> sync(Policy(100), image_sub, pos_cmd_sub, odom_sub);
 
-  // // message_filters::TimeSynchronizer<sensor_msgs::Image, quadrotor_msgs::PositionCommand> sync(image_sub, pos_cmd_sub, 100);
-  // sync.registerCallback(boost::bind(&callback, _1, _2, _3));
+  // message_filters::TimeSynchronizer<sensor_msgs::Image, quadrotor_msgs::PositionCommand> sync(image_sub, pos_cmd_sub, 100);
+  sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
   ros::spin();
 
